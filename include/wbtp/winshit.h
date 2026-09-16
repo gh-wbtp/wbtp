3964a06ddfde4e2376e9e5ac18cbb1c83a9deef4
boyninja1555 <flappyfloorg@gmail.com>
2026-09-16 01:33:31 -0500

    Now automating both static & dynamic builds!!

--- include/wbtp/winshit.h
+++ include/wbtp/winshit.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-#if defined(WBTP_BUILD)
+#if defined(WBTP_STATIC)
+#define WBTP_API
+#elif defined(WBTP_BUILD)
 #define WBTP_API __declspec(dllexport)
 #else
 #define WBTP_API __declspec(dllimport)
